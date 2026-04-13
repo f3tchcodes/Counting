44d7e2e93656f4f5246b492f2f3f77282104fb80
f3tch <f3tch.169@gmail.com>
2026-04-13 18:40:44 +0500

    Added dev commands

--- main.js
+++ main.js
@@ -20,38 +20,4 @@ loadCommands(client)
 loadEvents(client)
 
 
-// BOT SHUTDOWN MESSAGE
-const shutdown = async () => {
-    console.log("Shutting down, waiting for all messages to be sent!");
-
-    try {
-      const [rows] = await client.db.query(
-          "SELECT * FROM community_settings"
-      );
-
-      if (rows && rows.length > 0) {
-        for (const row of rows) {
-          try {
-            await client.channels.send(row.channel_id, "⚠️ **The bot will be offline for maintenance/updates. Halt counting!**")
-            const guild = client.guilds.cache.get(row.community_id);
-            console.log(`Sent message for: ${guild ? guild.name : row.community_id}`);
-          } catch (err) {
-            console.log(err)
-          }
-        }
-      }
-    } catch (err) {
-      console.log(err);
-    } 
-
-    await client.db.end();
-    console.log("Database closed. Goodbye!");
-    process.exit(0);
-};
-
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
-process.on('stopProcessId', shutdown)
-
-
 client.login(process.env.FLUXER_BOT_TOKEN)
