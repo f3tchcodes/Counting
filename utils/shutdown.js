b1517cbbf5801e51c8641c9110c4be4911131d43
f3tch <f3tch.169@gmail.com>
2026-04-14 10:13:46 +0500

    Updated dev.js

--- utils/shutdown.js
+++ utils/shutdown.js
@@ -1,25 +1,21 @@
 const pm2 = require("pm2");
 
-async function shutdown(message) {
+async function shutdown() {
     try {
-        console.log("Shutting down the bot...");
-        message.send("Shutting down the bot...")
-
         pm2.connect((err) => {
             if (err) {
                 console.log(err)
                 process.exit(1)
             }
 
-            const pmId = process.env.PM2_ID;
+            const pmId = process.env.pm_id;
 
             pm2.stop(pmId, (stopErr) => {
                 if (stopErr) return console.log(stopErr);
-                pm2.disconnect;
+                pm2.disconnect();
             });
         });
     } catch (err) {
-        message.send("Error occured while shutting down!");
         console.log(err);
     }
 }
