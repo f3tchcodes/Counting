821e8f393818443f0f5491789b5672f4764bf444
f3tch <f3tch.169@gmail.com>
2026-04-14 10:02:12 +0500

    Changes in dev.js

--- commands/dev.js
+++ commands/dev.js
@@ -93,7 +93,7 @@ module.exports = {
             `);
 
             try {
-                return shutdown();
+                return shutdown(message);
             } catch (err) {
                 console.log(err);
                 process.exit();
