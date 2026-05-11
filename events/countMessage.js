a61823f93284e452ac4a0265babd0d94aa5231b9
f3tch <f3tch.169@gmail.com>
2026-05-11 15:17:19 +0500

    already counted logs

--- events/countMessage.js
+++ events/countMessage.js
@@ -90,6 +90,8 @@ Guild Name: ${await client.guilds.fetch(message.guild.id).name},
 Author ID: ${message.author.id},
 Author Username: ${message.author.username},
 Current Count: ${current_count},
+Number Variable: ${number},
+Next Count: ${next_count},
 Time: ${new Date()}\n`);
                             } catch (err) {
                                 return console.log(err);
